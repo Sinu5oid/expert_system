cc2231aadefb5964b28a4321921f50d94db9c8a2
Andrew Gergert <axedrew33@gmail.com>
2019-11-23 01:00:20 +0600

    Refactor components to use module

--- src/components/file-loader.vue
+++ src/components/file-loader.vue
@@ -1,37 +1,73 @@
 <template>
-    <div class="ui placeholder segment" :class="[{loading: isLoading}]">
-        <div class="ui icon header">
-            <i class="file icon"></i>
-            Select a file
-        </div>
-        <div class="field">
-            <input name="inputFile" type="file" id="file_input" @change="selectFile($event)">
-        </div>
-    </div>
+  <div class="ui placeholder segment" :class="[{loading: isLoading}]">
+    <div class="ui icon header">
+      <i class="file icon"></i>
+        Select a file
+      </div>
+      <div class="field">
+        <input name="inputFile" type="file" id="file_input" @change="selectFile($event)">
+      </div>
+  </div>
 </template>
 <script>
+import { parseFromMkb, parseFromJson } from '../utils/json-helper.js';
+
 export default {
-    name: 'FileLoader',
-    data() {
-        return {
-            isLoading: false,
-        };
-    },
-    methods: {
-        selectFile(event) {
-            if (!event.target.files.length) {
-                return;
+  name: 'FileLoader',
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    selectFile(event) {
+      try {
+        if (!event.target.files.length) {
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = e => {
+          this.isLoading = true;
+          setTimeout(() => {
+            try {
+              const fileName = event.target.files[0].name;
+              if (!fileName.includes('.mkb') && !fileName.includes('.json')) {
+                throw Error('incompatible content');
+              }
+              const object = reader.result;
+              let json;
+              if (fileName.includes('.mkb')) {
+                const fromMkb = parseFromMkb(object);
+                json = JSON.stringify(fromMkb);
+                this.download(json, `${fileName.replace('.mkb', '')}.json`, 'application/json');
+                this.$emit('load', fromMkb);
+              } else {
+                json = parseFromJson(object);
+                this.$emit('load', json);
+              }
+            } catch (error) {
+              this.$emit('error', error.message);
+            } finally {
+              this.isLoading = false;
             }
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.isLoading = true;
-                setTimeout(() => {
-                    this.isLoading = false;
-                    this.$emit('load', e.target.result);
-                }, 500)
-            };
-            reader.readAsText(event.target.files[0], 'Windows-1251');
-        },
-    }
+          }, 500);
+        }
+        reader.readAsText(event.target.files[0], 'Windows-1251');
+      } catch (error) {
+        this.$emit('error', error.message);
+      }
+    },
+    download(content, fileName, contentType) {
+      try {
+        const a = document.createElement("a");
+        const file = new Blob([content], {type: contentType});
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+      } catch (error) {
+        this.$emit('error', error.message);
+      }
+    },
+  },
 }
 </script>
